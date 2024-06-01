1449d1cca675f4c554128d5502c8f9f5a73905c8
Life <geravanya963@gmail.com>
2024-06-01 21:52:44 +0300

    fix conflicts

--- GameJam/Assets/Scripts/src_ZombieLogic.cs
+++ GameJam/Assets/Scripts/src_ZombieLogic.cs
@@ -9,14 +9,14 @@ public class MobMovement : MonoBehaviour
     public float speed = 2.0f;
     public float rotationSpeed = 5f;
     public float detectionRange = 2.0f;
-<<<<<<< Updated upstream
+
     public string targetTag;
-=======
+
     public float attackDistance = 1f;
     public float attackReload = 0f;
 
     private bool isAttack = false;
->>>>>>> Stashed changes
+
 
     // Метод, вызываемый при входе другого коллайдера в триггер
     private void OnTriggerEnter(Collider other)
@@ -35,25 +35,7 @@ public class MobMovement : MonoBehaviour
     void Update()
     {
             float distanceToPlayer = Vector3.Distance(transform.position, player.position);
-<<<<<<< Updated upstream
-       
-            
-            if (distanceToPlayer <= detectionRange)
-            { 
-                
-                transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
 
-                Vector3 direction = (player.position - transform.position).normalized;
-                Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
-                transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
-           
-                
-            }
-        
-        }
-
-=======
-            
             if (distanceToPlayer <= detectionRange && !isAttack)
             {
                 if (distanceToPlayer > attackDistance + 0.7)
@@ -63,7 +45,7 @@ public class MobMovement : MonoBehaviour
                     Vector3 direction = (player.position - transform.position).normalized;
                     Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
                     transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
-            }
+                }
                 else
                 {
                     isAttack = true;
@@ -71,12 +53,14 @@ public class MobMovement : MonoBehaviour
                 }
             }
     }
+
     IEnumerator StartAttackDelay()
     {
         yield return new WaitForSeconds(0.5f);
         if (Vector3.Distance(transform.position, player.position) <= attackDistance + 0.7)
             Debug.Log("Атакован");
         isAttack = false;
->>>>>>> Stashed changes
     }
-}
+
+}   
+      
