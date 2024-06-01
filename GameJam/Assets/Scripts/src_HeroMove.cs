6e413fea1164a0392c1063ce5129c15668707df1
Life <geravanya963@gmail.com>
2024-06-01 12:33:13 +0300

    movement with animation

--- GameJam/Assets/Scripts/src_HeroMove.cs
+++ GameJam/Assets/Scripts/src_HeroMove.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class MovementHero : MonoBehaviour
@@ -5,6 +6,8 @@ public class MovementHero : MonoBehaviour
     [SerializeField] private Rigidbody _rb;
     [SerializeField] private float _speed = 5;
     [SerializeField] private float _turnSpeed = 360;
+    [SerializeField] private Animator anim;
+
 
     private Vector3 _input;
 
@@ -21,14 +24,43 @@ public class MovementHero : MonoBehaviour
     {
         Move();
     }
+
+    private Coroutine idleCoroutine;
     void GatherInput()
     {
         _input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+        if (_input != Vector3.zero)
+        {
+            anim.SetBool("isRun", true);
+            anim.SetBool("isWait", false);
+
+            if (idleCoroutine != null)
+            {
+                StopCoroutine(idleCoroutine);
+                idleCoroutine = null;
+            }
+        }
+        else
+        {
+            anim.SetBool("isRun", false);
+            if (idleCoroutine == null)
+            {
+                idleCoroutine = StartCoroutine(SetIdleAfterDelay(3.0f));
+            }
+        }
     }
+
+    IEnumerator SetIdleAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        anim.SetBool("isWait", true);
+    }
+
 
     void Look()
     {
-        if(_input !=Vector3.zero)
+        if (_input != Vector3.zero)
         {
 
             var relative = (transform.position + _input.ToIso()) - transform.position;
@@ -36,7 +68,7 @@ public class MovementHero : MonoBehaviour
 
             transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, _turnSpeed * Time.deltaTime);
         }
-        
+
     }
 
     void Move()
