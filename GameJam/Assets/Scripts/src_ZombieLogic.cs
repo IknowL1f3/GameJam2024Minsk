1999fd739fb7d8d037f5139b1a45b1698cccf6e3
Life <geravanya963@gmail.com>
2024-06-01 21:47:32 +0300

    -

--- GameJam/Assets/Scripts/src_ZombieLogic.cs
+++ GameJam/Assets/Scripts/src_ZombieLogic.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class MobMovement : MonoBehaviour
@@ -8,7 +9,14 @@ public class MobMovement : MonoBehaviour
     public float speed = 2.0f;
     public float rotationSpeed = 5f;
     public float detectionRange = 2.0f;
+<<<<<<< Updated upstream
     public string targetTag;
+=======
+    public float attackDistance = 1f;
+    public float attackReload = 0f;
+
+    private bool isAttack = false;
+>>>>>>> Stashed changes
 
     // Метод, вызываемый при входе другого коллайдера в триггер
     private void OnTriggerEnter(Collider other)
@@ -26,9 +34,8 @@ public class MobMovement : MonoBehaviour
 
     void Update()
     {
-        if (player != null)
-        {
             float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+<<<<<<< Updated upstream
        
             
             if (distanceToPlayer <= detectionRange)
@@ -45,5 +52,31 @@ public class MobMovement : MonoBehaviour
         
         }
 
+=======
+            
+            if (distanceToPlayer <= detectionRange && !isAttack)
+            {
+                if (distanceToPlayer > attackDistance + 0.7)
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+
+                    Vector3 direction = (player.position - transform.position).normalized;
+                    Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+                    transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+            }
+                else
+                {
+                    isAttack = true;
+                    StartCoroutine(StartAttackDelay());
+                }
+            }
+    }
+    IEnumerator StartAttackDelay()
+    {
+        yield return new WaitForSeconds(0.5f);
+        if (Vector3.Distance(transform.position, player.position) <= attackDistance + 0.7)
+            Debug.Log("Атакован");
+        isAttack = false;
+>>>>>>> Stashed changes
     }
 }
